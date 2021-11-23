c51f8014770f161df3989560ff7a8f2f54ad6f7c
Luis Lopez <luislopez1099lg@gmail.com>
2021-11-22 23:26:26 -0700

    wildcard added

--- main.py
+++ main.py
@@ -19,15 +19,7 @@ scoreDb = ScoreController()
 sessionDb = SessionController()
 categoryDb = CategoryController()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "https://typing-applg.netlify.app"
-
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
